a24921a63d19a2718534bc5dd770df6e3c1090fd
htetyadanar-hyo <149476965+htetyadanar-hyo@users.noreply.github.com>
2024-03-04 17:04:44 -0600

    Update ComputerMain.java

--- Assignment5Day2_3/ComputerMain.java
+++ Assignment5Day2_3/ComputerMain.java
@@ -3,9 +3,9 @@ package Assignment5Day2_3;
 public class ComputerMain {
 
     public static void main(String[] args) {
-        Computer c1 = new Computer("Microsoft","CPU 111",16,23);
-        Computer c2 = new Computer("Dell","CPU ..",8,123);
-        Computer c3 = new Computer("Microsoft","CPU 111",16,23);
+        Computer c1 = new Computer("Microsoft","i5",16,23);
+        Computer c2 = new Computer("Dell","i6",8,123);
+        Computer c3 = new Computer("Microsoft","i5",16,23);
         System.out.println(c1.equals(c2));
         System.out.println(c1.equals(c3));
         System.out.println(c1.hashCode());
